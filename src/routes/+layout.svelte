a577660296af47b0a3cfe998d0492814d0bc9f2c
Greatman Lim <greatman@saashop.fi>
2022-10-01 20:57:56 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,94 @@
+<div class="container">
+  <div class="container--header">
+    <nav class="header-left"></nav>
+    <nav class="header-middle"> 
+      <a href="/">etusivu</a>
+      <a href="/kurssit">kurssit</a>
+      <a href="/tapahtumat">tapahtumat</a>
+      <a href="/hallitus">hallitus</a>
+      <a href="/blogi">blogi</a>
+    </nav>
+    <nav class="header-right"></nav>
+  </div>
+
+  <main>
+    <slot></slot>
+  </main>
+
+  <footer class="container--footer">
+    <section>
+      <h3>Pohjalaisten tanssikerho ry</h3>
+      <nav class="footer-left">
+        <a href="/ry/yhdistyksen-saannot">Yhdistyksen säännöt</a>
+        <a href="/ry/liity-aktiiviksi">Liity aktiiviksi</a>
+        <a href="/ry/kannatusjasenyys">Kannatusjäsenyys</a>
+      </nav>
+    </section>
+      <section>
+      <h3>Yhteystiedot ja some</h3>
+      <nav class="footer-middle">
+        <a href="some/facebook">Facebook</a>
+        <a href="some/whatsapp">WhatsApp</a>
+        <a href="some/sahkopostilista">Sähköpostilista</a>
+        <a>pohjalaistentanssikerho.ry(ät)gmail.com</a>
+      </nav>
+    </section>
+      <section>
+      <h3>Osoite</h3>
+      <nav class="footer-right">
+        <span>Töölönkatu 3 A, 00910 Helsinki, 5krs.</span>
+        <a href="#">Avaa Reittioppaalla</a>
+        <a href="#">Avaa Google Mapsilla</a>
+      </nav>
+    </section>
+  </footer>
+</div>
+
+<style lang="scss">
+  .container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    height: 100vh;
+    & .container--header {
+      display: flex;
+      flex-wrap: nowrap;
+      justify-content: space-between;
+      align-items: center;
+      border-top: 3px solid var(--color-light-blue);
+      border-bottom: 3px solid var(--color-light-blue);
+      height: 3em;
+      background: var(--color-night-blue);
+      & .header-middle {
+        text-transform: uppercase;
+        a {
+          text-decoration: none;
+          color: var(--color-white);
+          transition: .3s;
+          margin: .9em;
+          font-size: .9em;
+          &:hover {
+            color: var(--color-orange);
+          }
+        }
+      }
+    }
+    & footer.container--footer {
+      flex: 1;
+      background-color: var(--color-night-blue);
+      border-top: 3px solid var(--color-light-blue);
+      display: flex;
+      justify-content: center;
+      gap: 1em;
+      & section {
+        & nav {
+          display: flex;
+          flex-wrap: nowrap;
+          flex-direction: column;
+        }
+      }
+      & nav {
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="jumbotron">
+  <h1>Pohjalaisten tanssikerho</h1>
+  <em>Opetusta, bileitä ja ekskursioita. Pohjalaisten tanssikerho tuo opiskelijan elämään tanssin iloa.</em>
+  <div class="sections">
+    <section>
+      <h2>Tiedotteet</h2>
+    </section>
+    <section>
+      <h2>Seuraavat kurssit</h2>
+    </section>
+    <section>
+      <h2>Tapahtumat</h2>
+    </section>
+  </div>
+</div>
+<div>
+  // main content
+</div>
+
+
+<style lang="scss">
+  .jumbotron {
+    background-image: url(img/picture/hanne-ilmari.jpg);
+    background-size: cover;
+    border-bottom: 3px solid var(--color-light-blue);
+    & h1 {
+      text-align: center;
+      font-size: 2.5em;
+      text-decoration: underline;
+      text-decoration-thickness: 5px;
+      text-decoration-color: orange;
+      padding-top: 1em;
+    }
+    & em {
+      display: block;
+      text-align: center;
+    }
+    & .sections {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 1em;
+      justify-content: center;
+      padding: 3em 0;
+      & section {
+        position: relative;
+        width: 300px;
+        height: 400px;
+        border: 2px solid var(--color-light-blue);
+        border-radius: 6px;
+        backdrop-filter: blur(6px);
+        background: #022140AF;
+        &::before {
+          content: '';
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          opacity: 70%;
+          border-radius: 4px;
+        }
+      }
+    }
+  }
+</style>

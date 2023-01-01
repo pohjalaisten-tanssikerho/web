c00137a0e8f66421d7a3625dec45e611a256e5e8
happla <104832348+happla@users.noreply.github.com>
2023-01-01 12:40:56 +1100

    add text to kurssi page

---
+++ src/routes/kurssit/+page.svelte
@@ -0,0 +1,22 @@
+<div class="container">
+	<div class="list">
+		<h3><a href="/">Yleistä kursseista</a></h3>
+		<p>Kaikille viikottaisille kursseille yhteistä asiaa ja käytäntöjä.</p>
+		<h3><a href="/">Kurssiaikataulut ja lajit</a></h3>
+		<p>Kaikkien viikottaisten kurssien aikataulut ja lajit.</p>
+		<h3><a href="/">Ilmoittautuminen</a></h3>
+		<p>Ilmoittaudu kursseille.</p>
+		<h3><a href="/">Hinnasto ja maksaminen</a></h3>
+		<p>Kaikkien kurssien hintatiedot ja maksuohjeet.</p>
+		<h1>Kurssitarjonta</h1>
+		<h3><a href="/">Alkeiskurssi</a></h3>
+		<p>Tanssin ensiasekeleet.</p>
+		<h3><a href="/">Alkeisjatko</a></h3>
+		<p>Vähän edistyneimmille. Kannattaa olla vähän myös lavatanssikokemusta.</p>
+		<h3><a href="/">Jatkokurssi</a></h3>
+		<p>
+			Pitkälle edistyneille tanssijoille. Täällä tanssitekniikka ja kuviot hiotaan yhä parempaan
+			kuntoon.
+		</p>
+	</div>
+</div>

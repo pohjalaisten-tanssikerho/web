36967be2acba64e858a12fb8ce38019ce21f86d5
happla <104832348+happla@users.noreply.github.com>
2023-01-03 17:10:41 +1100

    added working header pages and static pages

---
+++ src/routes/blogit/+page.svelte
@@ -0,0 +1,18 @@
+<div class="list">
+	<h3><a href="/blogit/kisaaminen/">Kisaaminen</a></h3>
+	<p>Kiinnostaako kisaaminen? Mitä pitää tehdä, että pääsee kisaamaan ja minkälaista se on.</p>
+	<h3><a href="/blogit/lavaetiketti/">Lavaetiketti</a></h3>
+	<p>Lavoilla on syytä osata käyttäytymissääntöjä, jotta kaikki voivat viihtyä.</p>
+	<h3><a href="/blogit/tanssilajit/">Tanssilajit</a></h3>
+	<p>
+		Lyhyt esittely tanssilajeista, mitä Pohjalaisten tanssikerho ja Hämäläis-Osakunnan tanssikerho
+		opettaa.
+	</p>
+	<h3><a href="/blogit/vastauksia-palautteisiin/">Vastauksia palautteisiin</a></h3>
+	<p>
+		Palautteiden antaminen on ollut turhan yksipuolista keskustelua. Nyt hallitus antaa vastauksia
+		palautteisiin.
+	</p>
+	<h3><a href="/blogit/vuosi-hallituksessa/">Vuosi hallituksessa</a></h3>
+	<p>Kokemuksia yhdistystoiminnasta.</p>
+</div>
---
+++ src/routes/hallitus/+page.svelte
@@ -0,0 +1,132 @@
+<h1 id="hallituksen-jäsenet">Hallituksen jäsenet</h1>
+<div class="board">
+	<div class="item">
+		<div class="hidden-description"><img src="/img/board/lasse-vaskelainen.jpg" /></div>
+		<div class="fill" />
+		<div class="center"><span class="role">puheenjohtaja</span></div>
+		<div class="center"><span class="name">Lasse Vaskelainen</span></div>
+		<div class="center"><div class="email">etunimi.sukunimi@gmail.com</div></div>
+	</div>
+	<div class="item">
+		<div class="hidden-description"><img src="/img/board/greatman-lim.jpg" /></div>
+		<div class="fill" />
+		<div class="center"><span class="role">varapuheenjohtaja</span></div>
+		<div class="center"><span class="name">Greatman Lim</span></div>
+		<div class="center"><div class="email">sukunimi.etunimi@gmail.org</div></div>
+	</div>
+	<div class="item">
+		<div class="hidden-description"><img src="/img/board/suvi-maenpaa.jpg" /></div>
+		<div class="fill" />
+		<div class="center"><span class="role">taloudenhoitaja</span></div>
+		<div class="center"><span class="name">Suvi Mäenpää</span></div>
+		<div class="center"><div class="email">zukkasd (ät) gmail . com</div></div>
+	</div>
+	<div class="item">
+		<div class="hidden-description"><img src="/img/board/anni-seilo.jpg" /></div>
+		<div class="fill" />
+		<div class="center"><span class="role">sihteeri</span></div>
+		<div class="center"><span class="name">Anni Seilo</span></div>
+		<div class="center"><div class="email">etu.suku@outlook.com</div></div>
+	</div>
+</div>
+<h1 id="järjestötoiminta">Järjestötoiminta</h1>
+<p>
+	Pohjalaisten tanssikerho toimii täysin vapaaehtoisvoimin. Palvelun on siis tuottanut tanssikerhon
+	jäsen tanssikerholaiselle. Yhdistyksen tavoite ei ole kerätä voittoa vaan tuottaa jäsenilleen
+	hyötyä.
+</p>
+<p>
+	Pohjalaisten tanssikerho on olemassa jäseniä varten, mutta ilman sitä pyörittävää hallitusta POT
+	ei olisi olemassa. Pohjalaisten tanssikerhon hallitus on yhdistyksen päättävä elin, jonka jäsenet
+	valitaan edellisen vuoden syyskokouksessa. Yhdistyksen säännöt ovat <a
+		href="/ry/yhdistyksen-saannot/">luettavissa omalla sivulla</a
+	>.
+</p>
+<h2 id="mukaan-hallitukseen">Mukaan hallitukseen?</h2>
+<p>
+	Hallituksessa toimiminen tuo vastuuta, mutta antaa mahdollisuuden vaikuttaa. Joskus työ voi olla
+	raskasta, mutta sen aikaan saamat onnistumisen tunteet ovat sen arvoisia. Muut hallituksen jäsenet
+	ovat auttamassa ja tukemassa. Asiat tehdään yhdessä. Hallituksessa oleminen on yksi
+	mielenkiintoisimpia elämän kokemuksia, jota myös työnantajat arvostavat.
+</p>
+<p>
+	Hieman henkilökohtaisemman tarinan voi lukea <a href="/blogi/vuosi-hallituksessa/"
+		>Hennan kirjoittamassa blogissa</a
+	>.
+</p>
+<p>Hyviä syitä liittyä hallitukseen:</p>
+<ol>
+	<li>merkintä CV:hen</li>
+	<li>halu vaikuttaa tanssikerhon toimintaan</li>
+	<li>halu auttaa</li>
+	<li>kokemus ja uusiin ihmisiin tutustuminen</li>
+</ol>
+<p>
+	Hallitus perehdyttää työtehtäviin, joten ei tarvitse pelätä, että kaikki on selvitettävä itse.
+</p>
+<h2 id="aktiivit">Aktiivit</h2>
+<p>
+	Hallitustoiminta tuo mukanaan vastuuta, mikä ei kaikille ehkä sovi. Aktiivit ovat joukko
+	tanssikerhon jäseniä, jotka auttavat yhdistyksen toiminnassa. Käytännössä he tekevät samaa kuin
+	hallitus ilman hallituksen vastuuta. Työtehtäviä on yllättävän paljon: tanssibileiden tarjoilu,
+	soittolistat, siivous jne.
+</p>
+<p>
+	Aktiiviksi voi ryhtyä milloin vain. Ota vain yhteyttä lähimpään hallituslaiseen ja kerro, että
+	voit auttaa. Otamme mielellämme kaiken avun vastaan!
+</p>
+<h1 id="yhteystiedot">Yhteystiedot</h1>
+<p>Hallituksen sähköposti: pohjalaistentanssikerho . ry (ät) gmail . com</p>
+<p>
+	Kirjepostin voi lähettää Pohjalaisen valtuuskunnan kautta lisäämällä kuoreen “TANSSIKERHO”.
+	Pohjalaisen valtuuskunnan osoite:
+</p>
+<p>Töölönkatu 3 A, 00940 Helsinki, 5krs.</p>
+
+<style>
+	.board {
+		display: grid;
+		grid-template-columns: repeat(4, auto);
+		grid-template-rows: repeat(2);
+		justify-items: center;
+		max-width: 1030px;
+		margin: 18px auto;
+		grid-row-gap: 2em;
+		margin-bottom: 26px;
+	}
+	.board .item {
+		position: relative;
+		background-color: #022140;
+		border-radius: 2px;
+		border: 2px solid #0b3c5d;
+		width: 214px;
+		margin-top: 0;
+	}
+	.board .item .hidden-description img {
+		position: absolute;
+		outline: 2px solid #0b3c5d;
+		outline-offset: -2px;
+		border-bottom: 3px solid #0b3c5d;
+		width: 214px;
+		height: 220px;
+	}
+	.board .item .fill {
+		height: 225px;
+	}
+	.board .item .center {
+		text-align: left;
+	}
+	.board .item .center .role {
+		font-size: 12px;
+	}
+
+	.board .item .center .name {
+		font-size: 16px;
+		font-weight: 700;
+		border-bottom: 2px solid #0b3c5d;
+	}
+	.board .item .email {
+		font-size: 14px;
+		padding: 10px 8px;
+	}
+</style>

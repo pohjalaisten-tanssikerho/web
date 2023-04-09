8e5078b8b142139aa5c4be8e8561e88af7562395
Hannu Pham <Hannu@Hannus-MacBook-Air.local>
2023-04-09 21:34:43 +0300

    -Added courses on the front page - has date: DDMMYY - needs css fixing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
-  import { PageData } from './$types'
-  export let data: PageData
-  let {coursesContainer}:PageData = data
+  import { onMount } from 'svelte';
+  import { loadCourses } from './page';
 
-  async function init() {
-    await loadCourses();
-    const courseTable = document.getElementById('courses-container').firstChild;
-    coursesContainer.innerHTML = '';
-    coursesContainer.appendChild(courseTable);
-  }
-
-  $: if (coursesLoaded) {
-    init();
-  }
+  onMount(() => {
+    loadCourses();
+  });
 </script>
-
 <div class="jumbotron">
   <h1>Pohjalaisten tanssikerho</h1>
   <em>Opetusta, bileitä ja ekskursioita. Pohjalaisten tanssikerho tuo opiskelijan elämään tanssin iloa.</em>
@@ -22,9 +13,9 @@
     <section>
       <h2>Tiedotteet</h2>
     </section>
-    <section class="course-section">
+    <section>
       <h2>Seuraavat kurssit</h2>
-      <div bind:this={coursesContainer}></div>
+      <div id="courses-container"></div>
     </section>
     <section>
       <h2>Tapahtumat</h2>
--- src/routes/page.ts
+++ src/routes/page.ts
@@ -8,9 +8,46 @@ dayjs.extend(timezone);
 const timezoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 export async function loadCourses() {
-  const response = await fetch('https://raw.githubusercontent.com/pohjalaisten-tanssikerho/web-page/master/data/courses.json');
-  const data = await response.json();
-  return data.courses;
-}
+  // Load course data
+  const courseData = await fetch('https://raw.githubusercontent.com/pohjalaisten-tanssikerho/web-page/master/data/courses.json').then(res => res.json());
+  const courses = courseData.courses;
+  console.log(courseData); //log the loaded course data
 
-export { timezoneName };
+  // Get container element
+  const container = document.getElementById('courses-container');
+
+  // Create table element
+  const table = document.createElement('table');
+
+  // Create table headers
+  const headerRow = document.createElement('tr');
+  const emptyHeaderCell = document.createElement('th');
+  headerRow.appendChild(emptyHeaderCell);
+  const courseDates = courses.slice(0, 3).map(course => {
+    const date = dayjs.utc(course.date).tz(timezoneName);
+    const dateHeaderCell = document.createElement('th');
+    dateHeaderCell.textContent = date.format('ddd, MMM D, YYYY h:mm A');
+    headerRow.appendChild(dateHeaderCell);
+    return date;
+  });
+  table.appendChild(headerRow);
+
+  // Create table rows for each course
+  const courseTypes = ['Alkeet', 'AlkeisJatko', 'Jatko'];
+  courseTypes.forEach(courseType => {
+    const courseRow = document.createElement('tr');
+    const courseTypeCell = document.createElement('th');
+    courseTypeCell.textContent = courseType;
+    courseRow.appendChild(courseTypeCell);
+    courseDates.forEach(date => {
+      const course = courses.find(course => dayjs.utc(course.date).tz(timezoneName).isSame(date));
+      const courseCell = document.createElement('td');
+      courseCell.textContent = course ? course[courseType.toLowerCase()] : '';
+      courseRow.appendChild(courseCell);
+    });
+    table.appendChild(courseRow);
+  });
+
+  // Add table to container
+  container.appendChild(table);
+}

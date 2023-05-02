58dfc1f60155ccf12cd5dbd9ebbc2d6ee09ead60
Hannu Pham <Hannu@Hannus-MacBook-Air.local>
2023-05-02 21:24:34 +0300

    price table updated to json file

--- src/routes/kurssit/hinnasto-ja-maksaminen/+page.svelte
+++ src/routes/kurssit/hinnasto-ja-maksaminen/+page.svelte
@@ -1,3 +1,22 @@
+<script>
+	export let prices;
+  
+	async function loadPrices() {
+	  const response = await fetch('https://raw.githubusercontent.com/pohjalaisten-tanssikerho/web-page/master/data/prices.json');
+	  const data = await response.json();
+	  prices = Object.fromEntries(
+		Object.entries(data).map(([category, values]) => [
+		  category,
+		  values.map((value) => ({ price: value.price, ref: value.ref })),
+		])
+	  );
+	}
+  
+	loadPrices();
+</script>
+
+
+
 <h1 id="hinnasto-ja-maksaminen">Hinnasto ja maksaminen</h1>
 <p>Maksu tapahtuu tilisiirrolla tilille:</p>
 <pre>
@@ -8,23 +27,35 @@
 </pre>
 <p>Muista käyttää viitettä!</p>
 <h2 id="kausikurssien-hinnasto">Kausikurssien hinnasto</h2>
+{#if prices}
 <table class="price-table">
-	<tbody
-		><tr><th colspan="2" /><th>Alkeet</th><th>Alkeisjatko</th><th>Jatko</th></tr><tr
-			><th rowspan="2">* VN<br />PPO<br />EPO</th><td class="dark-background">hinta</td><td
-				class="dark-background">35€</td
-			><td class="dark-background">40€</td><td class="dark-background">40€</td></tr
-		><tr><td>viite</td><td>1012</td><td>1119</td><td>1915</td></tr><tr
-			><th rowspan="2">**opiskelija</th><td class="dark-background">hinta</td><td
-				class="dark-background">55€</td
-			><td class="dark-background">60€</td><td class="dark-background">60€</td></tr
-		><tr><td>viite</td><td>1012</td><td>1119</td><td>1915</td></tr><tr
-			><th rowspan="2">muut</th><td class="dark-background">hinta</td><td class="dark-background"
-				>75€</td
-			><td class="dark-background">80€</td><td class="dark-background">80€</td></tr
-		><tr><td>viite</td><td>1012</td><td>1119</td><td>1915</td></tr></tbody
-	>
-</table>
+	<tbody>
+	  <tr>
+		<th colspan="2"></th>
+		<th>Alkeet</th>
+		<th>Alkeisjatko</th>
+		<th>Jatko</th>
+	  </tr>
+	  {#each Object.entries(prices) as [category, values]}
+		<tr>
+		  <th rowspan="2">{category}</th>
+		  <td class="dark-background">hinta</td>
+		  {#each values as value}
+			<td class="dark-background">{value.price}</td>
+		  {/each}
+		</tr>
+		<tr>
+		  <td>viite</td>
+		  {#each values as value}
+			<td>{value.ref}</td>
+		  {/each}
+		</tr>
+	  {/each}
+	</tbody>
+  </table>
+{:else}
+  <p>Hintoja päivitetään. Yritä myöhemmin uudelleen.</p>
+{/if}
 <p>Ota talteen kuitti ja pidä se mukana kursseilla, kunnes maksut on tarkistettu.</p>
 <div class="footnote">
 	* Osakuntalaisuudella tarkoitetaan seuraavia osakunnan jäseniä: <a href="http://vasa.nation.fi"
